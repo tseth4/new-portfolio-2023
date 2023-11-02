55e86d0d5eb2dd340e598af1f4eedc7562e917f7
ts <tristansetha@gmail.com>
2023-11-02 11:33:04 -0700

    sun and moon icon

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,10 +1,23 @@
 import "./NavStyles.scss";
+import Image from "next/image";
+import sun_icon from "@/public/sun.svg";
+import moon_icon from "@/public/moon.svg";
 
 export default function Nav() {
   return (
     <>
-      <div className="nav-side">Nav side</div>
-      <div className="nav-top">Nav top</div>
+      <div className="nav-side">side nav</div>
+      <div className="nav-top">
+        <div>
+          <Image
+            priority
+            width={20}
+            height={20}
+            src={moon_icon}
+            alt="Sun icon"
+          />
+        </div>
+      </div>
     </>
   );
 }
